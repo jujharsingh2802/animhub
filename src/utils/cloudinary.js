af513736fc253462e36d9f4e56d6ec24455317eb
jujharsingh2802 <sjujhar532@gmail.com>
2024-03-27 01:05:25 +0530

    project setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) {
+            console.log("Error LocalFilePath couldn't be found")
+            return null;
+        }
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto",
+        })
+        console.log("File is uploaded SUCCESSFULLY",response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //remove the locally saved file if upload fails
+        return null;
+    }
+}
+
+
