d9b1c91d00710486f2103eded7f4639c10d479f8
jujharsingh2802 <sjujhar532@gmail.com>
2024-04-02 00:21:13 +0530

    Subscription model added

---
+++ src/models/subscription.model.js
@@ -0,0 +1,14 @@
+import mongoose, {Schema} from "mongoose";
+
+const subscriptionSchema = new Schema({
+    subscriber:{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    chanel:{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+},{timestamps:true})
+
+const Subscription = mongoose.model('Subscription', subscriptionSchema);
