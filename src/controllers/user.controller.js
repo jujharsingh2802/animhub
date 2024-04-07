4663f292ea1028a5b9121488019c2005f9e88589
jujharsingh2802 <sjujhar532@gmail.com>
2024-04-07 23:54:41 +0530

    changing mongoDB operator

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
-
+ 
 const generateAccessandRefreshToken = async (userId) => {
   try {
     const user = await User.findById(userId);
@@ -159,8 +159,8 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1,
       },
     },
     {
@@ -424,7 +424,7 @@ const getWatchHistory = asyncHandler(async(req,res)=>{
           .json(new ApiResponse(
             200,
             user[0]?.watchHistory,
-            "User's watch History fetchedn SUCCESSFULLY!!"
+            "User's watch History fetched SUCCESSFULLY!!"
           ))
 })
 
