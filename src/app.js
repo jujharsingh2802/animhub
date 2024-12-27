5dd5933d0013a9e2f086fd791253b3462089155a
Jujhar Singh Sandhu <sjujhar532@gmail.com>
2024-12-27 23:56:47 +0530

    Increase request body size limits and ensure file uploads are handled in user routes

--- src/app.js
+++ src/app.js
@@ -9,11 +9,10 @@ app.use(cors({
     credentials: true,
 }));
 
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.json({limit: "16kb"}))
-app.use(express.static("public"))
-
-app.use(cookieParser())
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 //routes
 import userRouter from './routes/user.routes.js';
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -23,9 +23,9 @@ router.route("/login").post( upload.none(),loginUser)
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
-router.route("/change-password").post(verifyJWT, changePassword)
+router.route("/change-password").post(upload.none(), verifyJWT, changePassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser)
-router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+router.route("/update-account").patch(upload.none(), verifyJWT, updateAccountDetails);
 router.route("/update-avatar").patch(verifyJWT,upload.single("avatar"), updateUserAvatar)
 router.route("/update-cover-image").patch(verifyJWT,upload.single("coverImage"), updateUserCoverImage)
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
